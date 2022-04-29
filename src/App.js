55d213dc163a37b64dd4aa02ef07df9748c27224
LYK <dbsrudwin@gmail.com>
2022-04-29 12:53:56 +0900

    220429-React Board 리액트 기본 화면 UI 생성

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
-import AxiosTest from './AxiosTest';
+// import AxiosTest from './AxiosTest';
+import React from 'react';
+import { HashRouter, Route } from 'react-router-dom';
+import Home from './routes/Home';
+import Detail from './routes/Detail';
+import Write from './routes/Write';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
-    <div className="App">
-      <AxiosTest />
-    </div>
+    <HashRouter>
+      <Navigation />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/detail" exact={true} component={Detail} />
+      <Route path="/write" exact={true} component={Write} />
+    </HashRouter>
+    // <div className="App">
+    //   <AxiosTest />
+    // </div>
   );
 }
 
---
+++ src/routes/Home.js
@@ -0,0 +1,65 @@
+import React from 'react';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <header class="container">
+          <div class="p-5 mb-4 bg-light rounded-3 text-center">
+            <div class="container-fluid py-5">
+              <h1 class="display-5 fw-bold">React와 SpringBoot를 활용한 게시판</h1>
+              <p>목록 보기</p>
+            </div>
+          </div>
+        </header>
+        <main class="container">
+          <section>
+            <article class="row">
+              <div class="col">
+                <table class="table table-hover table-stripe text-center">
+                  <colgroup>
+                    <col width="10%" />
+                    <col width="40%" />
+                    <col width="15%" />
+                    <col width="15%" />
+                    <col width="10%" />
+                  </colgroup>
+                  <thead>
+                    <tr>
+                      <th>글 번호</th>
+                      <th>제목</th>
+                      <th>글쓴이</th>
+                      <th>등록시간</th>
+                      <th>조회수</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>1</td>
+                      <td>테스트제목</td>
+                      <td>테스터</td>
+                      <td>2022.04.29 12:50:00</td>
+                      <td>0</td>
+                    </tr>
+                    <tr>
+                      <td>2</td>
+                      <td>테스트제목</td>
+                      <td>테스터</td>
+                      <td>2022.04.29 12:50:00</td>
+                      <td>0</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </article>
+          </section>
+        </main>
+        <footer class="container-fluid py-5 bg-secondary opacity-50">
+          <p class="lead text-mute text-center">made by lyk</p>
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default Home;
